3e0ff1b01819db2cd3d96f5d56b4d38a85b24229
BhuviGoel <73011205+BhuviGoel@users.noreply.github.com>
2021-01-18 17:49:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,31 +17,31 @@ function draw() {
   strokeWeight(8);
  stroke(244, 4, 0);
  noFill();
- var secondA = map(sc,0,60,0,360);
- arc(0,0,300,300,0,secondA);
+ var scangle = map(sc,0,60,0,360);
+ arc(0,0,300,300,0,scangle);
 
  stroke(15, 245, 4);
- var minuteA = map(mn,0,60,0,360);
- arc(0,0,280,280,0,minuteA);
+ var mnangle = map(mn,0,60,0,360);
+ arc(0,0,280,280,0,mnangle);
 
  stroke(13, 0, 227);
- var hourA = map(hr%12,0,12,0,360);
- arc(0,0,260,260,0,hourA);
+ var hrangle = map(hr%12,0,12,0,360);
+ arc(0,0,260,260,0,hrangle);
 
  push();
- rotate(secondA);
+ rotate(scangle);
  stroke(244, 4, 0);
  line(0,0,100,0);
  pop();
 
  push();
- rotate(minuteA);
+ rotate(mnangle);
  stroke(15, 245, 4);
  line(0,0,75,0);
  pop();
 
  push();
- rotate(hourA);
+ rotate(hrangle);
  stroke(13, 0, 227);
  line(0,0,50,0);
  pop();
