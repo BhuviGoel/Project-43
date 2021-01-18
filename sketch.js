8e4e286ed7c9f1ed582e973a113502293dd761dc
BhuviGoel <73011205+BhuviGoel@users.noreply.github.com>
2021-01-18 17:48:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 
 function setup() {
   createCanvas(400,400);
-  angleMode(DEGREES);
+ // createSprite(400, 200, 50, 50);
+ angleMode(DEGREES);
 }
 
 function draw() {
@@ -13,37 +14,41 @@ function draw() {
  var mn= minute();
  var sc= second();
 
-strokeWeight(8);
+  strokeWeight(8);
  stroke(244, 4, 0);
  noFill();
- var scangle = map(sc,0,60,0,360);
- arc(0,0,300,300,0,scangle);
+ var secondA = map(sc,0,60,0,360);
+ arc(0,0,300,300,0,secondA);
 
  stroke(15, 245, 4);
- var mnangle = map(mn,0,60,0,360);
- arc(0,0,280,280,0,mnangle);
+ var minuteA = map(mn,0,60,0,360);
+ arc(0,0,280,280,0,minuteA);
 
  stroke(13, 0, 227);
- var hrangle = map(hr%12,0,12,0,360);
- arc(0,0,260,260,0,hrangle);
+ var hourA = map(hr%12,0,12,0,360);
+ arc(0,0,260,260,0,hourA);
 
  push();
- rotate(acangle);
+ rotate(secondA);
  stroke(244, 4, 0);
  line(0,0,100,0);
  pop();
 
  push();
- rotate(mnangle);
+ rotate(minuteA);
  stroke(15, 245, 4);
  line(0,0,75,0);
  pop();
 
  push();
- rotate(hrangle);
+ rotate(hourA);
  stroke(13, 0, 227);
  line(0,0,50,0);
  pop();
-
- drawSprites();
+ 
+ stroke(255);
+ point(0,0)
+ 
+  
+  drawSprites();
 }
